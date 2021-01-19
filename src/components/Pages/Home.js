7df5514293361e8fb3dad1cbd35d5a7834a20954
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-19 23:19:54 +0700

    add button

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Carousel from '../layouts/Carousel'
 import HighRated from './HighRated'
 
@@ -11,6 +12,12 @@ function Home() {
                 <div className="col s12 center">
                     <HighRated />
                 </div>
+            </div>
+            <hr className="container" style={{ border: '1px solid grey', borderRadius: 5 }} />
+            <div className="center">
+                <Link to='/movies' className="waves-effect waves-light btn-large orange accent-4 grey-text text-darken-3">
+                    <h5>SEE ALL</h5>
+                </Link>
             </div>
         </div>
     )
