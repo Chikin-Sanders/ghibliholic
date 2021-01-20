9e4321994db2fead2a5969d49de38a634bd80347
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-21 01:08:34 +0700

    add totoro image

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Carousel from '../layouts/Carousel'
 import HighRated from './HighRated'
+import totoro from '../images/totoro-walking.png'
 
 function Home() {
     return (
@@ -9,16 +10,19 @@ function Home() {
             <Carousel />
             <div className="row" style={{marginTop: 50}}>
                 <hr className="container" style={{ border: '1px solid grey', borderRadius: 5}} />
-                <div className="col s12 center" style={{ padding: 30 }}>
+                <div className="col s12 center" style={{ padding: 40 }}>
                     <HighRated />
                 </div>
             </div>
             <hr className="container" style={{ border: '1px solid grey', borderRadius: 5 }} />
-            <div className="center">
+            <div className="center" style={{margin: '50px 0'}}>
+                <img src={totoro} alt="totoro" height="400"/>
+                <h4 className="grey-text text-darken-4">Find out other great movies</h4>
                 <Link to='/movies' className="waves-effect waves-light btn-large orange accent-4 grey-text text-darken-3">
-                    <h5>SEE ALL</h5>
+                    <h5 className="white-text">SEE ALL</h5>
                 </Link>
             </div>
+            <hr className="container" style={{ border: '1px solid grey', borderRadius: 5 }} />
         </div>
     )
 }
