85fb3ed14daab7541f2171c4730d39b11837cf4e
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-21 17:13:35 +0700

    add styling

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -4,26 +4,41 @@ import Carousel from '../layouts/Carousel'
 import HighRated from './HighRated'
 import totoro from '../images/totoro-walking.png'
 
+const breakStyle = {
+    border: '1px solid grey',
+    borderRadius: 5
+}
 function Home() {
     return (
-        <div>
+        <div className="grey-text text-darken-4">
             <Carousel />
             <div className="row" style={{marginTop: 50}}>
-                <hr className="container" style={{ border: '1px solid grey', borderRadius: 5}} />
+                <hr className="container" style={{ breakStyle }} />
                 <div className="col s12 center" style={{ padding: 40 }}>
                     <HighRated />
                 </div>
             </div>
-            <hr className="container" style={{ border: '1px solid grey', borderRadius: 5 }} />
-            <div className="center" style={{margin: '30px 0', padding: 30}}>
-                <img src={totoro} alt="totoro" height="400"/>
-                <br/>
-                <br/>
-                <Link to='/movies' className="hoverable waves-effect waves-light btn-large orange accent-4 grey-text text-darken-3">
-                    <h5 className="white-text">Find out other great films</h5>
-                </Link>
+            <hr className="container" style={{ breakStyle }} />
+            <div className="center" style={{margin: '15px 0', padding: 30}}>
+                <div className="row container">
+                    <div className="col s6">
+                        <h6 style={{ textAlign: 'justify' }}>
+                            Studio Ghibli is world respected studio animation. 
+                            By watching the film you will see the magic they created for you.
+                        </h6>
+                        <br/>
+                        <br/>
+                        <Link to='/films' className="hoverable right waves-effect waves-light btn-small orange accent-4 white-text">
+                            Find out more
+                        </Link>
+                    </div>
+                    <div className="col s6">
+                    <img src={totoro} alt="totoro" height="210"/>
+                    </div>
+                </div>
             </div>
-            <hr className="container" style={{ border: '1px solid grey', borderRadius: 5 }} />
+            <hr className="container" style={{ breakStyle }} />
+            
         </div>
     )
 }
