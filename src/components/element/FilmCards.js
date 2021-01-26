f939be0bdcb4262f0e57a555841769ee86a7f30a
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-26 13:57:48 +0700

    adding file to display all film

---
+++ src/components/element/FilmCards.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function FilmCards({ posts, loading}) {
+    if (loading){
+        return(
+            <div className="center">
+                <div className="preloader-wrapper big active">
+                    <div className="spinner-layer spinner-blue">
+                        <div className="circle-clipper left">
+                            <div className="circle"></div>
+                        </div><div className="gap-patch">
+                            <div className="circle"></div>
+                        </div><div className="circle-clipper right">
+                            <div className="circle"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="row">
+            <div className="col s12">
+                {
+                    posts.map(post => {
+                        return(
+                            <div className="card">
+                                <div className="card-content">
+                                    <span className="card-title">{post.title}</span>
+                                    <hr />
+                                    <p>{post.description}</p>
+                                </div>
+                            </div>
+                            )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default FilmCards
