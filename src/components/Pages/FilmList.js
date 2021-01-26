e1d0adc7e87aeed53638efc182bcb80b20459daa
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-26 15:25:50 +0700

    adding pagination

--- src/components/Pages/FilmList.js
+++ src/components/Pages/FilmList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import FilmCards from '../element/FilmCards'
+import Pagination from '../element/Pagination'
 import axios from 'axios'
 
 export class FilmList extends Component {
     state = {
         posts: [],
-        loading: false
+        loading: false,
+        currentPage: 1,
+        postsPerPage: 5
     }
 
     componentDidMount(){
@@ -21,13 +24,25 @@ export class FilmList extends Component {
         fetchPosts()
     }
 
+
     render() {
-        console.log(this.state.posts)
+        const { loading, postsPerPage, posts } = this.state
+        var { currentPage } = this.state
+        const indexOfLastPost = currentPage * postsPerPage
+        const indexOfFirstPost = indexOfLastPost - postsPerPage
+        const currentPost = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+        function paginate(pageNumbers){
+            currentPage = pageNumbers
+            console.log(currentPage)
+        }
+
         return (
             <div className='container'>
                 <label htmlFor="search-bar"><h4>Find your film:</h4></label>
-                <FilmCards className="center-align" posts={this.state.posts} loading={this.state.loading} />
-            </div>
+                <FilmCards className="center-align" posts={currentPost} loading={loading} />
+                <Pagination totalPosts={posts.length} postsPerPage={postsPerPage} paginate={paginate} />
+            </div> 
         )
     }
 }
