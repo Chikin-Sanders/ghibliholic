2f7d2d6e94bfb2eac95a7efec2cd5ae69f9df937
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-26 13:58:30 +0700

    adding film list

--- src/components/Pages/FilmList.js
+++ src/components/Pages/FilmList.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import FilmCards from '../element/FilmCards'
+import axios from 'axios'
 
 export class FilmList extends Component {
+    state = {
+        posts: [],
+        loading: false
+    }
+
+    componentDidMount(){
+        const fetchPosts = async () => {
+            this.setState({ loading: true })
+            const res = await axios.get('https://ghibliapi.herokuapp.com/films')
+            this.setState({
+                posts: res.data,
+                loading: false
+            })
+        }
+
+        fetchPosts()
+    }
+
     render() {
+        console.log(this.state.posts)
         return (
             <div className='container'>
-                <h1>This is film list</h1>
+                <label htmlFor="search-bar"><h4>Find your film:</h4></label>
+                <FilmCards className="center-align" posts={this.state.posts} loading={this.state.loading} />
             </div>
         )
     }
