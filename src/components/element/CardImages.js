b86331ea5079b98636322d052431ccecbf43fcb2
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-27 23:46:26 +0700

    changing to functional component

--- src/components/element/CardImages.js
+++ src/components/element/CardImages.js
@@ -1,88 +1,74 @@
-import React, { Component } from 'react'
+import { React, useEffect, useState } from 'react'
 import axios from 'axios'
 
-export class CardImages extends Component {
-    state = {
-        images: {},
-        loading: false
-    }
-    spaceToPlus() {
-        const { title, release_date } = this.props.film
-        const year = release_date
-        const newTitle = []
-        for(var count = 0; count < title.length; count++){
-            var char = title.charAt(count).toLowerCase()
-            if( char === " ") {
-                newTitle.push("+")
-            } else {
-                newTitle.push(char)
+function CardImages ({ film, height }) {
+    const [image, setImage] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [title, setTitle] = useState('')
+    const [year, setYear] = useState('')
+    console.log('from cardImages.js the title is: '+film.title)
+
+    useEffect(() => {
+        const spaceToPlus = () => {
+            const newTitle = []
+            for (var count = 0; count < film.title.length; count++) {
+                var char = film.title.charAt(count).toLowerCase()
+                if (char === " ") {
+                    newTitle.push("+")
+                } else {
+                    newTitle.push(char)
+                }
             }
+            setTitle(newTitle.join(''))
+            setYear(film.release_date)
         }
-        return [newTitle.join(''), year]
-    }
-    componentDidMount() {
-        const BASE_URL = 'https://api.themoviedb.org/3/search/movie?api_key=a71c653291a8f11727eb5d8df0043e5b&language=en-US&query='
-        const YEAR_QUERY = '&page=1&include_adult=false&primary_release_year='
+        
         const fetchImage = async() => {
-            this.setState({
-                loading: true
-            })
-            const res = await axios.get(BASE_URL + this.spaceToPlus()[0] + YEAR_QUERY + this.spaceToPlus()[1])
+            const BASE_URL = 'https://api.themoviedb.org/3/search/movie?api_key=a71c653291a8f11727eb5d8df0043e5b&language=en-US&query='
+            const YEAR_QUERY = '&page=1&include_adult=false&primary_release_year='
+            setLoading(true)
+            const res = await axios.get(BASE_URL + title + YEAR_QUERY + year)
             const posterPath = res.data.results[0].poster_path
-            this.setState({
-                images: 'https://image.tmdb.org/t/p/w500/' + posterPath,
-                loading: false
-            })
+            setImage('https://image.tmdb.org/t/p/w500/' + posterPath)
+            setLoading(false)
         }
+        spaceToPlus()
         fetchImage()
-        // .then((result) =>{
-        //     var posterPath = result.data.results[0].poster_path
-        //     this.setState({
-        //         images: 'https://image.tmdb.org/t/p/w500/' + posterPath
-        //     })
-            
-        // })
-        // .catch((err) => {
-        //     console.log(err)
-        // })
+    }, [film, title, year])
+    
+    const imgStyle = {
+        height: height, 
+        objectFit: 'contain' 
     }
-    render() {
-        const { height } = this.props
-        const { title } = this.props.film
-        const imgStyle = {
-            height: height, 
-            objectFit: 'contain' 
-        }
-        if(this.state.loading){
-            return(
-                <div className = "center valigned" >
-                    <div className="preloader-wrapper big active">
-                        <div className="spinner-layer spinner-blue">
-                            <div className="circle-clipper left">
-                                <div className="circle"></div>
-                            </div><div className="gap-patch">
-                                <div className="circle"></div>
-                            </div><div className="circle-clipper right">
-                                <div className="circle"></div>
-                            </div>
+
+    if(loading){
+        return(
+            <div className = "center valigned" >
+                <div className="preloader-wrapper big active">
+                    <div className="spinner-layer spinner-blue">
+                        <div className="circle-clipper left">
+                            <div className="circle"></div>
+                        </div><div className="gap-patch">
+                            <div className="circle"></div>
+                        </div><div className="circle-clipper right">
+                            <div className="circle"></div>
                         </div>
                     </div>
-                </div >
-            )
-        } else if(title.length){
-            return(
-                <>
-                    <img 
-                    src={this.state.images} 
-                    alt={title} 
-                    style={ imgStyle } 
-                    className="responsive-img activator" />
-                        <span className="card-title activator grey-text text-darken-3">
-                    </span>
-                </>
-            )
-        }
+                </div>
+            </div >
+        )
     }
+    return(
+        <>
+            <img 
+            src={image} 
+            alt={title} 
+            style={ imgStyle } 
+            className="responsive-img activator" />
+                <span className="card-title activator grey-text text-darken-3">
+            </span>
+        </>
+    )
 }
 
 export default CardImages
