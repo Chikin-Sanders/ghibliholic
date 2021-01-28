1c0af93327487d7a00b4716e62e4cdbc0450ed55
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-29 00:05:02 +0700

    change footer color

--- src/components/Pages/Footer.js
+++ src/components/Pages/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Footer extends Component {
     render() {
         return (
-            <div className="center blue-grey lighten-3" style={{ padding: '10px 0', marginTop: 100, marginBottom: 0 }}>
+            <div className="center blue-grey lighten-1" style={{ padding: '10px 0', marginTop: 100, marginBottom: 0 }}>
                 <h6>&#169;2021 - Aziz Hussein Z</h6>
             </div>
         )
